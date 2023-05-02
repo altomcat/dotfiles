e7117c0fee9b35c3ac542358447f81d957eb352f
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2023-05-02 21:14:01 +0200

    Updated channels.scm with my own channel

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,13 +1,22 @@
 (cons* (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
-	;;(commit "637e728111993c06d5f0acd47156a653251994cd")
-	)
+	(introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
        (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
 	;;(commit "9cb35c02164d929fcb8929e7f454df215df8cf25")
 	)
+       (channel
+	(name 'my-guix-packages)
+	;; (url "git@github.com:altomcat/my-guix-packages.git")
+        (url "https://github.com/altomcat/my-guix-packages.git")
+	(commit "71d1ed6147dd9299de4a25eafde3057ec51d8e6c")
+	(branch "master"))
        ;; Add pyros to Guix default channels.
        ;; (channel
        ;;  (name 'pyros)
