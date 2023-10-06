a78863a57641a31152280f1e1b48a4fb38cb2346
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2023-10-06 21:15:24 +0200

    Comment RDE repository from Guix channels.

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -15,12 +15,12 @@
        ;; 	  (openpgp-fingerprint
        ;; 	   "AA52 B3F3 E764 5F95 6BA1 23E2 43D7 A3F7 7931 A85D")))
        ;; 	)
-       (channel
-	(name 'rde)
-	(url "https://git.sr.ht/~abcdw/rde")
-	(introduction
-	 (make-channel-introduction
-	  "257cebd587b66e4d865b3537a9a88cccd7107c95"
-	  (openpgp-fingerprint
-	   "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+       ;; (channel
+       ;; 	(name 'rde)
+       ;; 	(url "https://git.sr.ht/~abcdw/rde")
+       ;; 	(introduction
+       ;; 	 (make-channel-introduction
+       ;; 	  "257cebd587b66e4d865b3537a9a88cccd7107c95"
+       ;; 	  (openpgp-fingerprint
+       ;; 	   "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
        %default-channels)
