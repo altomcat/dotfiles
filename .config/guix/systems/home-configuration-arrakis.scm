b10a2c95daceb73f6dd21d7b75d20824282cce8a
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2023-11-05 19:31:49 +0100

    Split list of packages into various files.

--- .config/guix/systems/home-configuration-arrakis.scm
+++ .config/guix/systems/home-configuration-arrakis.scm
@@ -8,262 +8,139 @@
 	     (gnu home services shells)
              (gnu home services ssh)
              (gnu home services desktop)
+	     (gnu home services shepherd)
              (gnu packages)
 	     (gnu packages xdisorg)
              (gnu services)
              (guix gexp)
              (gnu home services shells))
 
+(load "emacs.scm")
+(load "sway.scm")
+
 (home-environment
  ;; Below is the list of packages that will show up in your
  ;; Home profile, under ~/.guix-home/profile.
  (packages
   (specifications->packages
-   (list
-    ;; fonts
-    "font-iosevka-comfy"
-    "font-openmoji"
-    "font-awesome"
-    "unicode-emoji"
-    "font-google-noto-emoji"
+   (append 
+    (list
+     ;; fonts
+     "font-iosevka-comfy"
+     "font-openmoji"
+     "font-awesome"
+     "unicode-emoji"
+     "font-google-noto-emoji"
 
-    ;; useful tools
-    "aspell"
-    "aspell-dict-en"
-    "aspell-dict-fr"
+     ;; useful tools
+     "aspell"
+     "aspell-dict-en"
+     "aspell-dict-fr"
+     "ripgrep"
+     "unzip"
+     "p7zip"
+     "curl"
+     "tree"
+     "gnupg"
+     "pinentry"
+     "pinentry-emacs"
+     "fd"
+     "file"
+     "rmlint"				; remove duplicates
+     "yt-dlp"
+     "xdg-utils" ;; for xdg-open
+     "xeyes" ;; to find Xwayland apps
+     "mpv"
+     "colord"
 
-    "alacritty"
-    "fuzzel"
-    "ripgrep"
-    "unzip"
-    "curl"
-    "tree"
-    "gnupg"
-    "pinentry"
-    "pinentry-emacs"
-    "thunar"
-    "thunar-volman"
-    "neofetch"
-    "xdg-utils" ;; for xdg-open
-    "dconf" ;; store connections for virt-manager
-    "xeyes" ;; to find Xwayland apps
-    "mpv"
-    "slurp"
-    "grim"
-    "swappy" ;; screenshot for sway
-    "colord"
+     ;; utils
+     "lsof"
+     "ntfs-3g"
+     "pasystray"
+     "radeon-firmware"
+     "brightnessctl"
+     "broadcom-bt-firmware"
+     
+     ;; dev
+     "git"
+     "automake"
+     "autoconf"
+     "gcc-toolchain@12"
+     "rust"
+     "cmake"
+     "python"
+     "shellcheck"
+     "fish"
+     
+     ;; webrowser
+     "ungoogled-chromium-wayland"
+     "icecat"
 
-    ;; utils
-    "ntfs-3g"
+     ;; themes
+     "hicolor-icon-theme"
+     "adwaita-icon-theme" ;; for virt-manager icons
+     "matcha-theme"
+     "papirus-icon-theme"
 
-    ;; dev
-    
-    ;; webrowser
-    "vimb"
-    "ungoogled-chromium"
-    "ungoogled-chromium-wayland"
+     ;; Virtualization
+     "virt-manager"
+     "libvirt"
+     "python-libvirt"
+     "bridge-utils"
+     "dconf" ;; store connections for virt-manager
+     "qemu"
+     
+     "gnome-tweaks"
+     "gnome-themes-extra"
+     "volumeicon"
+     "mpv"
+     "pavucontrol"
 
-    ;; themes
-    "hicolor-icon-theme"
-    "adwaita-icon-theme" ;; for virt-manager icons
-    "matcha-theme"
-    "papirus-icon-theme"
+     ;; torrent
+     "transmission"
+     "transmission-remote-gtk"
 
-    ;; sway
-    "wofi" ;; launcher apps
-    "bemenu" ;; alternative menu for sway
-    "swayidle"
-    "swaynotificationcenter"
-    "wl-clipboard"
+     ;; misceallenous apps
+     "gnome-bluetooth"
+     "adwaita-icon-theme"
+     "guile-ac-d-bus"    
+     "xdot"
+     "peek"				; screencast    
+     "graphviz"
+     "sxiv"
+     "gnome-shell-extensions"
+     "scrot"
+     "xftwidth"
+     "xsetroot"
+     "xhost"
+     "xwininfo"
+     "setxkbmap"
+     "xmodmap"
+     
+     ;; locales
+     "glibc-locales"
+     
+     ;; libraries
+     "ffmpeg"
+     "libpng"
+     "zstd"
+     "gtk+:bin"
+     "gtk+"
 
-    ;; Virtualization
-    "virt-manager"
-    "libvirt"
-    "python-libvirt"
+     ;; mpd server/client
+     "mpd"
+     "mpd-mpc" ;; mpc used by ncmpcpp to send notification
+     "ncmpcpp"
+     "cli-visualizer" ;; aka vis
 
-    ;; emacs
-    "emacs-pgtk"
-    "emacs-substitute"
-    "emacs-js2-mode"
-    "emacs-hydra"
-    "emacs-helpful"
-    "emacs-graphviz-dot-mode"
-    "emacs-graphql"
-    "emacs-glsl-mode"
-    ;; emacs-gif-screencast
-    "emacs-geiser-guile"
-    "emacs-geiser"
-    "emacs-guix"
-    "emacs-magit"
-    "emacs-flycheck"
-    "emacs-expand-region"
-    "emacs-emojify"
-    "emacs-elfeed"
-
-    "emacs-ef-themes"
-    "emacs-doom-themes"
-    "emacs-modus-themes"
-    "emacs-doom-modeline"
-    "emacs-dashboard"
-    "emacs-corfu"
-    "emacs-consult-org-roam"
-    
-    "emacs-consult"
-    "emacs-bongo"
-    "emacs-all-the-icons-dired"
-    "emacs-all-the-icons-completion"
-    "emacs-all-the-icons"
-    "emacs-ace-window"
-    "emacs-websocket"
-    "emacs-simple-httpd"
-    "emacs-visual-fill-column"
-
-    "emacs-orderless"
-    "emacs-vertico"
-    "emacs-marginalia"
-    "emacs-embark"
-
-    "emacs-popper"
-    
-    "emacs-org"
-    "emacs-org-superstar"
-    "emacs-citar"
-    "emacs-citar-org-roam"
-    "emacs-org-fancy-priorities"
-    "emacs-org-roam-ui"
-    "emacs-saveplace-pdf-view"
-    "emacs-pdf-tools"
-    "emacs-org-noter"
-    "emacs-org-appear"
-    "emacs-org-tree-slide"
-    "emacs-org-download"
-    "emacs-org-drill"
-    "emacs-toc-org"
-    "emacs-org-pomodoro"
-    "emacs-ox-reveal"
-    "emacs-plantuml-mode"
-    "emacs-org-present"
-    "emacs-tablist"
-    "emacs-simple-httpd"
-    "emacs-sml-mode"
-    "emacs-which-key"
-    "emacs-perspective"
-    "emacs-beacon"
-    "emacs-keycast"
-    "emacs-rainbow-delimiters"
-    "emacs-highlight-indent-guides"
-    "emacs-org-fragtog"
-    "emacs-yasnippet"
-    "emacs-yasnippet-snippets"
-    "emacs-nov-el"
-    "emacs-multiple-cursors"
-    "emacs-smartparens"
-    "emacs-aggressive-indent"
-
-    "emacs-ccls"
-    "emacs-dap-mode"
-    "emacs-pyvenv"
-    "emacs-jupyter"
-
-    "emacs-ement"
-    ;; "emacs-julia-mode"
-    "emacs-julia-repl"
-    "emacs-typescript-mode"
-    "emacs-json-mode"
-    "emacs-js2-mode"
-
-    "emacs-lua-mode"
-    "emacs-cmake-mode"
-    "emacs-powershell"
-
-    "emacs-kotlin-mode"
-    "emacs-cape"
-    "emacs-tempel"
-    "emacs-tempel-collection"
-    
-    "emacs-compat"
-    "emacs-transient"
-    "emacs-eros"
-    "emacs-shackle"
-    "emacs-kind-icon"
-    "emacs-org-contrib"
-    "emacs-mpv"
-    "emacs-aio"
-    "emacs-font-lock+"
-    "tree-sitter"
-
-    "lsof"
-    "gnome-tweaks"
-    "qemu"
-    "icecat"
-    "grim"
-    "swappy"
-    "youtube-dl"
-    "gnome-themes-extra"
-    "volumeicon"
-    "transmission-remote-gtk"
-    "mpv"
-    "ffmpeg"
-    "network-manager-openvpn"
-    "pavucontrol"
-    "blueman"
-    ;;"dunst"
-    "transmission"
-    "gnome-bluetooth"
-    "xdot"
-    "peek"
-    "pasystray"
-    "arandr"
-    "gtk+:bin"
-    "protonvpn-cli"
-    "gtk+"
-    "graphviz"
-    "git"
-    "rmlint"
-    "sxiv"
-    "screenfetch"
-    "gnome-shell-extensions"
-    "scrot"
-    "xftwidth"
-    "xsetroot"
-    "fish"
-    "python"
-    "xhost"
-    "unclutter"
-    "xprop"
-    "xev"
-    "setxkbmap"
-    "xmodmap"
-    "neofetch"
-    "gcc-toolchain@12"
-    "rust"
-    "cmake"
-    "playerctl"
-    "openvpn"
-    "radeon-firmware"
-    "brightnessctl"
-    "adwaita-icon-theme"
-    "shellcheck"
-    "hlint"
-    "htop"
-    "libpng"
-    "p7zip"
-    "btop"
-    "fd"
-    "broadcom-bt-firmware"
-    "stow"
-    "unzip"
-    "xwininfo"
-    "guile-ac-d-bus"
-    "glibc-locales"
-    "automake"
-    "autoconf"
-    "file"
-    "cmatrix"
-    "zstd"
-    )
-   )
-  )
+     ;; ASCII's art stuff
+     "cmatrix"
+     "neofetch"
+     "htop"
+     "btop"
+     "neofetch")
+    %my-sway-pkgs
+    %my-emacs-pkgs)))
 
  ;; Below Is the list of Home services.  To search for available
  ;; services, run 'guix home search KEYWORD' in a terminal.
@@ -271,9 +148,11 @@
   (list (service home-bash-service-type
                  (home-bash-configuration
                   (guix-defaults? #f)
+		  (aliases
+                   '(("youtube-dl" . "yt-dlp")))
 		  (bashrc (list (local-file
 				 "/home/altomcat/guix-config/.bashrc"
-				 "bashrc")))
+				 "bashrc"))) 
 		  (bash-profile (list (local-file
 				       "/home/altomcat/guix-config/.bash_profile"
 				       "bash_profile")))
@@ -282,9 +161,10 @@
 					   ("EDITOR" . "emacsclient -t")
 					   ("VISUAL" . "emacsclient -c -a emacs")))))
 	
-	(service home-redshift-service-type
-		 (home-redshift-configuration
-		  (location-provider 'manual)
-		  (latitude 49.0)
-		  (longitude 1.2)))))
- )
+	;; (service home-redshift-service-type
+	;; 	 (home-redshift-configuration
+	;;           (location-provider 'manual)
+	;; 	  (adjustment-method 'wayland)
+	;; 	  (latitude 49.0)
+	;; 	  (longitude 1.2)))
+	)))
---
+++ .config/guix/systems/sway.scm
@@ -0,0 +1,23 @@
+
+(define %my-sway-pkgs
+  (list
+   ;; sway
+   "wofi" ;; launcher apps
+   "bemenu" ;; alternative menu for sway
+   "swayidle"
+   "wl-clipboard"
+   "slurp"
+   "grim"
+   "swappy" ;; screenshot for sway
+   "thunar"
+   "thunar-volman"
+   "alacritty"
+   "fuzzel"
+   "gucharmap" ;; view Unicode characters table
+   "libnotify" ;; notify-send
+   "swaynotificationcenter";; Sway notification 
+   "wf-recorder" ;; Screencast recorder for sway
+   "kanshi" ;; replacement for arandr
+   ;; "arandr"
+   "redshift-wayland"
+   "blueman"))
