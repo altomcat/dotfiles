281e68cedc320c1f43892e2983968cf60a692dad
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2024-06-27 07:31:53 +0200

    Added OVMF package for libvirt.

--- .config/guix/arrakis-cfg.scm
+++ .config/guix/arrakis-cfg.scm
@@ -11,7 +11,7 @@
              (nongnu system linux-initrd))
 
 (use-service-modules linux dbus cups virtualization docker desktop networking ssh spice xorg)
-(use-package-modules gnome emacs vpn virtualization cups spice wm)
+(use-package-modules gnome emacs vpn virtualization cups spice wm firmware)
 
 (define %my-desktop-services
   (modify-services %desktop-services
@@ -44,130 +44,135 @@
 		   ;; 					   %default-sysctl-settings))))
 		   ))
 (operating-system
- ;; Use non-free Linux and firmware
- (kernel linux)
- (initrd microcode-initrd)
- (firmware (cons* broadcom-bt-firmware
-		  linux-firmware
-  		  %base-firmware))
- (kernel-arguments
-  (cons* "video=3440x1440@50" %default-kernel-arguments))
- (locale "fr_FR.utf8")
- (timezone "Europe/Paris")
- (keyboard-layout (keyboard-layout "us" "intl"))
- (host-name "arrakis")
+  ;; Use non-free Linux and firmware
+  (kernel linux)
+  (initrd microcode-initrd)
+  (firmware (cons* broadcom-bt-firmware
+		   linux-firmware
+  		   %base-firmware))
+  (kernel-arguments
+   (cons* "video=3440x1440@50" %default-kernel-arguments))
+  (locale "fr_FR.utf8")
+  (timezone "Europe/Paris")
+  (keyboard-layout (keyboard-layout "us" "intl"))
+  (host-name "arrakis")
 
- (locale-definitions  (list (locale-definition
-			     (name "en_US.utf8")
-			     (source "en_US"))
-			    (locale-definition
-			     (name "fr_FR.utf8")
-			     (source "fr_FR"))))
+  (locale-definitions  (list (locale-definition
+			      (name "en_US.utf8")
+			      (source "en_US"))
+			     (locale-definition
+			      (name "fr_FR.utf8")
+			      (source "fr_FR"))))
 
- (users (cons* (user-account
-		(name "altomcat")
-		(comment "Arnaud Lechevallier")
-		(group "users")
-		(home-directory "/home/altomcat")
-		(supplementary-groups
-                 '("wheel"
-		   "netdev"
-                   "kvm"       ;;  from DW config
-                   "tty"       ;;  from DW config
-                   "input"     ;;  from DW config
-                   ;;"docker"    ;;  from DW config
-                   ;;"realtime"  ;;  from DW config
-		   "audio"
-		   "video"
-		   "lp"         ;; "lp to let user configure the device"
-		   "libvirt")))
-               %base-user-accounts))
- (packages
-  (append
-   (map specification->package
-	'(
-	  "sway"
-          "ntfs-3g"
-	  "exfat-utils"
-	  "fuse-exfat"
-          "xterm"
-          "pulseaudio"
-	  "gvfs"
-	  "tlp"
-	  "wireguard-tools"
-          "chili-sddm-theme"
-	  ))
-   %base-packages))
+  (users (cons* (user-account
+		 (name "altomcat")
+		 (comment "Arnaud Lechevallier")
+		 (group "users")
+		 (home-directory "/home/altomcat")
+		 (supplementary-groups
+                  '("wheel"
+		    "netdev"
+                    "kvm"       ;;  from DW config
+                    "tty"       ;;  from DW config
+                    "input"     ;;  from DW config
+                    ;;"docker"    ;;  from DW config
+                    ;;"realtime"  ;;  from DW config
+		    "audio"
+		    "video"
+		    "lp"         ;; "lp to let user configure the device"
+		    "libvirt")))
+		%base-user-accounts))
+  (packages
+   (append
+    (map specification->package
+	 '(
+	   "sway"
+           "ntfs-3g"
+	   "exfat-utils"
+	   "fuse-exfat"
+           "xterm"
+           "pulseaudio"
+	   "gvfs"
+	   "tlp"
+	   "wireguard-tools"
+           "chili-sddm-theme"
+	   "ovmf" 
+	   ))
+    %base-packages))
 
- ;;(kernel-loadable-modules (list wireguard-linux-compat))
- (services
-  (append
+  ;;(kernel-loadable-modules (list wireguard-linux-compat))
+  (services
+   (append
+    (list
+     (service openssh-service-type)
+     (service bluetooth-service-type)
+     (service gnome-desktop-service-type)
+     (service sddm-service-type
+	      (sddm-configuration
+	       (theme "chili")
+               (remember-last-user? #t)
+	       ))
+     (service libvirt-service-type
+	      (libvirt-configuration
+	       (unix-sock-group "libvirt")
+	       (tls-port "16555")))
+     (service virtlog-service-type)
+     (service cups-service-type
+              (cups-configuration
+               (web-interface? #t)
+               (extensions
+		(list cups-filters))))
+     (simple-service 'wireguard-module
+		     kernel-module-loader-service-type
+		     '("wireguard"))
+     (service spice-vdagent-service-type)
+     (service screen-locker-service-type
+	      (screen-locker-configuration
+	       (name "swaylock")
+	       (program (file-append swaylock "/bin/swaylock"))
+	       (using-pam? #t)
+	       (using-setuid? #f)))
+     (extra-special-file "/usr/share/OVMF/OVMF_CODE.fd"
+                         (file-append ovmf "/share/firmware/ovmf_code_x64.bin"))
+     (extra-special-file "/usr/share/OVMF/OVMF_VARS.fd"
+                         (file-append ovmf "/share/firmware/ovmf_vars_x64.bin"))
+     ;; (service mpd-service-type
+     ;;          (mpd-configuration
+     ;; 	      (user "altomcat")
+     ;;           (music-directory "/home/altomcat/Musique")))
+     ;; (set-xorg-configuration
+     ;;  (xorg-configuration
+     ;;   (keyboard-layout keyboard-layout)))
+     )
+    %my-desktop-services))
+
+  (bootloader
+   (bootloader-configuration
+    (bootloader grub-efi-bootloader)
+    (targets (list "/boot/efi"))
+    (keyboard-layout keyboard-layout)))
+  (swap-devices
    (list
-    (service openssh-service-type)
-    (service bluetooth-service-type)
-    (service gnome-desktop-service-type)
-    (service sddm-service-type
-	     (sddm-configuration
-	      (theme "chili")
-              (remember-last-user? #t)
-	      ))
-    (service libvirt-service-type
-	     (libvirt-configuration
-	      (unix-sock-group "libvirt")
-	      (tls-port "16555")))
-    (service virtlog-service-type)
-    (service cups-service-type
-             (cups-configuration
-              (web-interface? #t)
-              (extensions
-               (list cups-filters))))
-    (simple-service 'wireguard-module
-		    kernel-module-loader-service-type
-		    '("wireguard"))
-    (service spice-vdagent-service-type)
-    (service screen-locker-service-type
-	     (screen-locker-configuration
-	      (name "swaylock")
-	      (program (file-append swaylock "/bin/swaylock"))
-	      (using-pam? #t)
-	      (using-setuid? #f)))
-    ;; (service mpd-service-type
-    ;;          (mpd-configuration
-    ;; 	      (user "altomcat")
-    ;;           (music-directory "/home/altomcat/Musique")))
-    ;; (set-xorg-configuration
-    ;;  (xorg-configuration
-    ;;   (keyboard-layout keyboard-layout)))
-    )
-   %my-desktop-services))
+    (swap-space
+     (target (uuid "94d6d8e8-6de7-42bb-b3c6-ffaff76053d2")))))
+  (file-systems
+   (cons* (file-system
+            (mount-point "/boot/efi")
+            (device (uuid "1C1D-53CE" 'fat32))
+            (type "vfat"))
+          (file-system
+            (mount-point "/")
+            (device
+             (uuid "a94722e5-5b7d-4f5e-b678-296085c800aa"
+                   'ext4))
+            (type "ext4"))
+          (file-system
+            (mount-point "/home")
+            (device
+             (uuid "500a431d-816d-4272-a822-ffb06bba0daa"
+                   'ext4))
+            (type "ext4"))
+          %base-file-systems))
 
- (bootloader
-  (bootloader-configuration
-   (bootloader grub-efi-bootloader)
-   (targets (list "/boot/efi"))
-   (keyboard-layout keyboard-layout)))
- (swap-devices
-  (list
-   (swap-space
-    (target (uuid "94d6d8e8-6de7-42bb-b3c6-ffaff76053d2")))))
- (file-systems
-  (cons* (file-system
-          (mount-point "/boot/efi")
-          (device (uuid "1C1D-53CE" 'fat32))
-          (type "vfat"))
-         (file-system
-          (mount-point "/")
-          (device
-           (uuid "a94722e5-5b7d-4f5e-b678-296085c800aa"
-                 'ext4))
-          (type "ext4"))
-         (file-system
-          (mount-point "/home")
-          (device
-           (uuid "500a431d-816d-4272-a822-ffb06bba0daa"
-                 'ext4))
-          (type "ext4"))
-         %base-file-systems))
-
- ;; Allow resolution of '.local' host names with mDNS
- (name-service-switch %mdns-host-lookup-nss))
+  ;; Allow resolution of '.local' host names with mDNS
+  (name-service-switch %mdns-host-lookup-nss))
