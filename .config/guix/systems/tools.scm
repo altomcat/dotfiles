b2553100aa2a915bdb6f503996b9db5f49219307
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2024-11-10 15:46:16 +0100

    Moved guix package related to tools.

--- .config/guix/systems/tools.scm
+++ .config/guix/systems/tools.scm
@@ -9,22 +9,18 @@
    "gnupg"
    "pinentry"
    "pinentry-emacs"
-   "fd"
    "file"
    "rmlint" ; remove duplicates
    "yt-dlp"
    "xdg-utils" ; for xdg-open
    "xeyes" ; to find Xwayland apps
    "colord"
-<<<<<<< HEAD
    "fd" ; for consult-denote-find
-   "notmuch"
-=======
+
    ;; mail imap
    "notmuch"
    "isync"
->>>>>>> 8702f07 (Added new packages.)
-
+   
    ;; utils
    "lsof"
    "ntfs-3g"
