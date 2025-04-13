43587b5e4bac804affd88e0f5099cf405a955492
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2025-04-13 12:49:41 +0200

    Cleanup and remove gvfs.

--- .config/guix/arrakis-cfg.scm
+++ .config/guix/arrakis-cfg.scm
@@ -15,34 +15,24 @@
 
 (define %my-desktop-services
   (modify-services %desktop-services
-		   (delete gdm-service-type)
-		   ;; (gdm-service-type config =>
-		   ;; 		     (wayland? #t))
-		   (network-manager-service-type config =>
-						 (network-manager-configuration (inherit config)
-										(vpn-plugins (list network-manager-openvpn))))
-		   ;; Authorize substitute from nonguix channel
-		   (guix-service-type config => (guix-configuration
-						 (inherit config)
-						 (substitute-urls
-						  (append (list "https://substitutes.nonguix.org")
-							  %default-substitute-urls))
-						 (authorized-keys
-						  (append (list (plain-file "non-guix.pub"
-									    "(public-key (ecc \n
+    (delete gdm-service-type)
+    ;; (gdm-service-type config =>
+    ;; 		     (wayland? #t))
+    (network-manager-service-type config =>
+				  (network-manager-configuration (inherit config)
+								 (vpn-plugins (list network-manager-openvpn))))
+    ;; Authorize substitute from nonguix channel
+    (guix-service-type config => (guix-configuration
+				  (inherit config)
+				  (substitute-urls
+				   (append (list "https://substitutes.nonguix.org")
+					   %default-substitute-urls))
+				  (authorized-keys
+				   (append (list (plain-file "non-guix.pub"
+							     "(public-key (ecc \n
                                                                                   (curve Ed25519)\n
                                                                                   (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))\n"))
-							  %default-authorized-guix-keys))))
-		   ;; (dbus config => (dbus-root-service-type
-		   ;; 		    (services (list blueman))))
-		   ;; (sysctl-service-type config =>
-		   ;; 			(sysctl-configuration
-		   ;; 			 (settings (append '(("net.ipv6.conf.enp0s25.disable_ipv6" . "1")
-		   ;; 					     ("net.ipv6.conf.enp3s0.disable_ipv6" . "1")
-		   ;; 					     ("net.ipv6.conf.all.disable_ipv6" . "1")
-		   ;; 					     ("net.ipv6.conf.default.disable_ipv6" . "1"))
-		   ;; 					   %default-sysctl-settings))))
-		   ))
+					   %default-authorized-guix-keys))))))
 (operating-system
   ;; Use non-free Linux and firmware
   (kernel linux)
@@ -72,12 +62,10 @@
 		 (supplementary-groups
                   '("wheel"
 		    "netdev"
-                    "kvm"       ;;  from DW config
-                    "tty"       ;;  from DW config
-                    "input"     ;;  from DW config
-                    ;;"docker"    ;;  from DW config
-                    ;;"realtime"  ;;  from DW config
-		    "audio"
+                    "kvm"
+                    "tty"
+                    "input"
+                    "audio"
 		    "video"
 		    "lp"         ;; "lp to let user configure the device"
 		    "libvirt")))
@@ -92,7 +80,7 @@
 	   "fuse-exfat"
            "xterm"
            "pulseaudio"
-	   "gvfs"
+	   ;;"gvfs"
 	   "tlp"
 	   "wireguard-tools"
 	   "chili-sddm-theme"
@@ -100,7 +88,6 @@
 	   ))
     %base-packages))
 
-  ;;(kernel-loadable-modules (list wireguard-linux-compat))
   (services
    (append
     (list
@@ -136,15 +123,7 @@
      (extra-special-file "/usr/share/OVMF/OVMF_CODE.fd"
                          (file-append ovmf "/share/firmware/ovmf_code_x64.bin"))
      (extra-special-file "/usr/share/OVMF/OVMF_VARS.fd"
-                         (file-append ovmf "/share/firmware/ovmf_vars_x64.bin"))
-     ;; (service mpd-service-type
-     ;;          (mpd-configuration
-     ;; 	      (user "altomcat")
-     ;;           (music-directory "/home/altomcat/Musique")))
-     ;; (set-xorg-configuration
-     ;;  (xorg-configuration
-     ;;   (keyboard-layout keyboard-layout)))
-     )
+                         (file-append ovmf "/share/firmware/ovmf_vars_x64.bin")))
     %my-desktop-services))
 
   (bootloader
